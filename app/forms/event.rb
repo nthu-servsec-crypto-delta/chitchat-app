53d7c3599fe78ccbc78ecb14e0a3e5f9b2f3c317
Tony Yang <t510599@gmail.com>
2024-06-14 10:16:43 +0800

    fix(Validation): do type check with coercion

--- app/forms/event.rb
+++ app/forms/event.rb
@@ -7,13 +7,13 @@ module ChitChat
     # Form validation for create/edit event
     class Event < AppContract
       params do
-        required(:name).filled(:str?)
-        optional(:descritpion).filled(:str?)
-        required(:longitude).filled(:str?) # TODO: fix type: float
-        required(:latitude).filled(:str?)
-        required(:radius).filled(:str?)
-        required(:start_time).filled(:str?) # TODO: fix type: date_time
-        required(:end_time).filled(:str?)
+        required(:name).filled(:string)
+        optional(:descritpion).filled(:string)
+        required(:longitude).filled(:float, gt?: -180, lt?: 180)
+        required(:latitude).filled(:float, gt?: -180, lt?: 180)
+        required(:radius).filled(:string)
+        required(:start_time).filled(:date_time)
+        required(:end_time).filled(:date_time)
       end
     end
   end
--- app/forms/postit.rb
+++ app/forms/postit.rb
@@ -7,9 +7,9 @@ module ChitChat
     # Form validation for create postit
     class Postit < AppContract
       params do
-        optional(:message).filled(:str?)
-        required(:longitude).filled(:str?) # TODO: fix type: float
-        required(:latitude).filled(:str?)
+        optional(:message).filled(:string)
+        required(:longitude).filled(:float, gt?: -180, lt?: 180)
+        required(:latitude).filled(:float, gt?: -180, lt?: 180)
       end
     end
   end
