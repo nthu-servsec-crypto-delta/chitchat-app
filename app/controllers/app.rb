0c7b1d1078749f01227211cc2d16947b77097cab
Tony Yang <t510599@gmail.com>
2024-06-21 02:12:04 +0800

    feat: better time input

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -8,7 +8,7 @@ module ChitChat
   class App < Roda
     plugin :render, engine: 'slim', views: 'app/presentation/views'
     plugin :assets, css: { base: 'style.css', map: 'map.css' },
-                    js: { postits: 'postits.js', event_detail: 'event_detail.js', map: 'map.js' },
+                    js: { postits: 'postits.js', events: 'events.js', event_detail: 'event_detail.js', map: 'map.js' },
                     path: 'app/presentation/assets', group_subdirs: false
     plugin :public, root: 'app/presentation/public'
     plugin :multi_route
